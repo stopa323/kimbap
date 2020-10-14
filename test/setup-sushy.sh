b20219d4cf2955b9e479f5390bc01ac331256bf9
stopa323 <stopa323@gmail.com>
2020-10-14 18:39:55 +0300

    Add script for setting up local testing env

---
+++ test/setup-sushy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if virsh list --all --name | grep -qw "vbmc-node"; then
+    echo "Cleaning existing vbmc-node..."
+    virsh destroy vbmc-node
+    virsh undefine vbmc-node
+fi
+
+virt-install                \
+    --name vbmc-node        \
+    --ram 1024              \
+    --disk size=1           \
+    --vcpus 2               \
+    --os-type linux         \
+    --os-variant fedora28   \
+    --graphics vnc          \
+    --print-xml > domain.xml
+
+virsh define domain.xml
+rm -f domain.xml
+
+# Start BMC emulator
+sushy-emulator --port 8000 --libvirt-uri "qemu:///system"
